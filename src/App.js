50856308d035d2628dc3e39b5446a9e8b35c611f
Eddy Chen <eddychen615@gmail.com>
2023-09-23 21:58:05 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const splashTimeout = setTimeout(() => {
+      setShowSplash(false);
+    }, 1500);
+
+    const logoTimeout = setTimeout(() => {
+      // Add your logic for fading out the logo here
+    }, 1200);
+
+    return () => {
+      clearTimeout(splashTimeout);
+      clearTimeout(logoTimeout);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showSplash && (
+        <div id="splash" className="splash">
+          {/* Your splash content goes here */}
+        </div>
+      )}
+      <div className={showSplash ? 'hidden' : 'appear'}>
+        {/* Content to show after splash is hidden */}
+      </div>
     </div>
   );
 }
